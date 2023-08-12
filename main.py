ac3914633becd6dc61d56b9783f9672fdfed54c8
gopancakes <samuelfdove@gmail.com>
2023-08-12 10:48:52 -0500

    adding heroku stuff

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import asyncio
 import chess
 import json
+import os
 import secrets
+import signal
 from stockfish import Stockfish
 import websockets
 
@@ -56,6 +58,12 @@ async def handler(websocket):
 
 
 async def main():
+    # Set the stop condition when receiving SIGTERM.
+    loop = asyncio.get_running_loop()
+    stop = loop.create_future()
+    loop.add_signal_handler(signal.SIGTERM, stop.set_result, None)
+
+    port = int(os.environ.get("PORT", "8001"))
     async with websockets.serve(handler, "", 8001):
         await asyncio.Future()  # run forever
 
